c1d60621c21c2e5ac85d8854e0783439cd0028ea
JayDub21 <justin.wofford.21@gmail.com>
2020-11-21 07:06:07 -0600

    helmet & morgan

--- express-basic/app.js
+++ express-basic/app.js
@@ -1,14 +1,16 @@
+const helmet = require('helmet');
+const morgan = require('morgan');
 const Joi = require('joi');
 const logger = require('./logger');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
-
-// app.use(express.urlencoded());
-
 // Direct Link to file in public folder - http://localhost:5000/readme.txt
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('tiny'));
 
 app.use(logger);
 
