191bbd5cb66b944f2f290f826d957152b4298541
JayDub21 <justin.wofford.21@gmail.com>
2020-11-24 12:20:36 -0600

    mongoose schema & model

--- mongo-basic/index.js
+++ mongo-basic/index.js
@@ -3,3 +3,27 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/demo')
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB...', err))
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Node.js Course',
+        author: 'Mosth', 
+        tags: ['node', 'backend'],
+        isPublished: true
+    });
+    
+    const result = await course.save();
+    console.log(result);
+}
+
+createCourse();
