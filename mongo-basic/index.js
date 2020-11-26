d744c5c022e39e958f4c9af195095a90aeb1775f
JayDub21 <justin.wofford.21@gmail.com>
2020-11-25 18:01:37 -0600

    logical query operation

--- mongo-basic/index.js
+++ mongo-basic/index.js
@@ -16,9 +16,9 @@ const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse() {
     const course = new Course({
-        name: 'Node.js Course',
-        author: 'Mosth', 
-        tags: ['node', 'backend'],
+        name: 'Angular Course',
+        author: 'Mosh', 
+        tags: ['angular', 'frontend'],
         isPublished: true
     });
     
@@ -26,4 +26,20 @@ async function createCourse() {
     console.log(result);
 }
 
-createCourse();
+async function getCourses() {
+
+
+    const courses = await Course
+    //Put find() before .or([]) & .and([])
+    .find()
+    .or([ {author: 'Mosh'}, { isPublished: true }])
+
+    // .find({author: 'Mosth', isPublished: true})
+    .limit(10)
+    .sort({name: 1 })
+    .select({ name: 1, tags: 1});
+    console.log(courses);
+}
+
+// createCourse();
+getCourses();
