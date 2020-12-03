000c0a59bd3f7dedc80734f85d9f898554a4a513
JayDub21 <justin.wofford.21@gmail.com>
2020-12-03 14:52:18 -0600

    unit tests and renamed folders

--- unit-testing/tests/lib.test.js
+++ unit-testing/tests/lib.test.js
@@ -1,4 +1,6 @@
 const lib = require('../lib');
+const db = require('../db');
+const mail = require('../mail');
 
 // Testing numbers ↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 describe('absolute', () => {
@@ -75,5 +77,26 @@ describe('registerUser', () => {
     });
 });
 // Mock functions ↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-
+describe('applyDiscount', () => {
+    it('should apply 10% discount if customer has more than 10 points', () => {
+        db.getCustomerSync = function (customerId) {
+            console.log('Fake reading customer...');
+            return { id: customerId, points: 20 }; 
+        }
+        const order = { customerId: 1, totalPrice: 10 };
+        lib.applyDiscount(order);
+        expect(order.totalPrice).toBe(9);
+    });
+});
 // Mock functions ↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+describe('nofityCustomer', () => {
+    it('should send an email to the customer', () => {
+    db.getCustomerSync = jest.fn().mockReturnValue({ email: 'a' });
+    mail.send = jest.fn()
+
+    lib.notifyCustomer({ customerId: 1 });
+
+    expect(mail.send).toHaveBeenCalled();
+    expect(mail.send.mock.calls[0][1]).toMatch(/order/);
+    });
+});
